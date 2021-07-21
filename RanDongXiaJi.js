0d671ee032e3f32d93c5501b2aebcb9a8fdd36d0
loong <codeloong@foxmail.com>
2021-07-21 09:33:47 +0800

    修复bug

--- RanDongXiaJi.js
+++ RanDongXiaJi.js
@@ -11,11 +11,11 @@ function start() {
         sleep(3000);
         while (textContains("运动+").exists()) {
             textContains("运动+").findOnce().click();
-            toast("收集运动卡币");sleep(3000);
+            toast("收集运动卡币"); sleep(3000);
         }
         while (textContains("任务+").exists()) {
             textContains("任务+").findOnce().click();
-            toast("收集任务卡币");sleep(3000);
+            toast("收集任务卡币"); sleep(3000);
         }
     }
     doTask();
@@ -36,13 +36,13 @@ function doTask() {
             sleep(3000);
             for (var i = 0; i < 5; i++) {
                 className("android.view.View").scrollable(true).depth(15).findOne().child(i).child(0).child(4).click();
-                toast("加购第" + (i+1) + "个商品");
+                toast("加购第" + (i + 1) + "个商品");
                 sleep(5000);
                 back();
                 sleep(3000);
             }
             toast("浏览加购完成，返回");
-        }  else if (textContains("浏览").exists() && textContains("浏览").findOnce().parent().child(8).text() == "去完成") {
+        } else if (textContains("浏览").exists() && textContains("浏览").findOnce().parent().child(8).text() == "去完成") {
             textContains("浏览").findOnce().parent().child(8).click();
             sleep(3000);
             toast("浏览完成，返回");
@@ -50,10 +50,10 @@ function doTask() {
             textStartsWith("参与可得").findOnce().parent().child(8).click();
             sleep(3000);
             toast("浏览完成，返回");
-        }else if (textContains("成功入会").exists() && textContains("成功入会").findOnce().parent().child(8).text() == "去完成") {
+        } else if (textContains("成功入会").exists() && textContains("成功入会").findOnce().parent().child(8).text() == "去完成") {
             textContains("成功入会").findOnce().parent().child(8).click();
             sleep(3000);
-            if(textContains("加入店铺会员").exists()){
+            if (textContains("加入店铺会员").exists()) {
                 toast("脚本结束（涉及个人隐私,请手动加入店铺会员或者忽略加入会员任务)");
                 break;
             }
@@ -66,16 +66,15 @@ function doTask() {
         sleep(1000);//给提示一个显示的时间
         back();
         sleep(3000);
-        for(var i=0;!className("android.widget.Image").text("c143642ad0850f7a").exists()&&i<5;i++){
-            toast("返回");
+        for (var i = 1; !className("android.widget.Image").text("c143642ad0850f7a").exists() && i <= 5; i++) {
+            if (i == 5) {
+                toast("无法返回任务界面,脚本结束");
+                exit();
+            }
+            toast("无法返回任务界面,第" + i + "次尝试返回(最多4次)");
             sleep(1000);
             back();
             sleep(3000);
-            if(i==5){
-                toast("无法返回任务界面,请重新执行脚本");
-                exit();
-            }
         }//避免弹窗,返回任务界面
     }
-    
 }
